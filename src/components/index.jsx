917260e7897de7c143c6d40bf86923134ba3db5c
kenny <105208736+devkennyy@users.noreply.github.com>
2022-06-14 21:19:19 +1000

    update index.jsx
    removed es lint comment

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,7 +1,6 @@
 export { default as Button } from './Button';
 export { default as ThemeSettings } from './ThemeSettings';
 export { default as Sidebar } from './Sidebar';
-// eslint-disable-next-line import/no-cycle
 export { default as Navbar } from './Navbar';
 export { default as Footer } from './Footer';
 export { default as Cart } from './Cart';
