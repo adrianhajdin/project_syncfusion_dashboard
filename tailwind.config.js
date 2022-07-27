57e363e597f9940792a3c4815dfde95d8714b0af
baxy5 <bakos.soda@gmail.com>
2022-07-27 09:53:20 +0200

    #3 unreaded message style fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,45 +1,46 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}'],
-  darkMode: 'class',
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
   theme: {
     fontFamily: {
-      display: ['Open Sans', 'sans-serif'],
-      body: ['Open Sans', 'sans-serif'],
+      display: ["Open Sans", "sans-serif"],
+      body: ["Open Sans", "sans-serif"],
     },
     extend: {
       fontSize: {
-        14: '14px',
+        14: "14px",
       },
       backgroundColor: {
-        'main-bg': '#FAFBFB',
-        'main-dark-bg': '#20232A',
-        'secondary-dark-bg': '#33373E',
-        'light-gray': '#F7F7F7',
-        'half-transparent': 'rgba(0, 0, 0, 0.5)',
+        "main-bg": "#FAFBFB",
+        "main-dark-bg": "#20232A",
+        "secondary-dark-bg": "#33373E",
+        "light-gray": "#F7F7F7",
+        "half-transparent": "rgba(0, 0, 0, 0.5)",
+        "orange": "#fec90f",
       },
       borderWidth: {
-        1: '1px',
+        1: "1px",
       },
       borderColor: {
-        color: 'rgba(0, 0, 0, 0.1)',
+        color: "rgba(0, 0, 0, 0.1)",
       },
       width: {
-        400: '400px',
-        760: '760px',
-        780: '780px',
-        800: '800px',
-        1000: '1000px',
-        1200: '1200px',
-        1400: '1400px',
+        400: "400px",
+        760: "760px",
+        780: "780px",
+        800: "800px",
+        1000: "1000px",
+        1200: "1200px",
+        1400: "1400px",
       },
       height: {
-        80: '80px',
+        80: "80px",
       },
       minHeight: {
-        590: '590px',
+        590: "590px",
       },
       backgroundImage: {
-        'hero-pattern':
+        "hero-pattern":
           "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')",
       },
     },
