5d49b0bf3a62fbe8b6365c22fa8a626d6f7bc708
Adrian Hajdin - JavaScript Mastery <contact@jsmastery.pro>
2022-08-02 11:49:52 +0200

    refactor

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,7 +40,7 @@ module.exports = {
       },
       backgroundImage: {
         'hero-pattern':
-          "url('https://demos.wrappixel.com/premium-admin-templates/react/flexy-react/main/static/media/welcome-bg-2x-svg.25338f53.svg')",
+          "url('https://i.ibb.co/MkvLDfb/Rectangle-4389.png')",
       },
     },
   },
