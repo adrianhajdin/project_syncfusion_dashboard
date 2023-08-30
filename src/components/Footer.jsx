a5847519563ad5404e96c6c270462bb8ccf925ce
koen de vulder <devulderk@gmail.com>
2023-08-30 20:23:52 +0200

    feat: updated cards kanban

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <div className="mt-24">
     <p className="dark:text-gray-200 text-gray-700 text-center m-20">
-      © 2022 All rights reserved by Shoppy.com
+      © 2023 All rights reserved by Andro
     </p>
   </div>
 );
--- src/pages/Kanban.jsx
+++ src/pages/Kanban.jsx
@@ -4,21 +4,39 @@ import { KanbanComponent, ColumnsDirective, ColumnDirective } from '@syncfusion/
 import { kanbanData, kanbanGrid } from '../data/dummy';
 import { Header } from '../components';
 
-const Kanban = () => (
-  <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
-    <Header category="App" title="Kanban" />
-    <KanbanComponent
-      id="kanban"
-      keyField="Status"
-      dataSource={kanbanData}
-      cardSettings={{ contentField: 'Summary', headerField: 'Id' }}
-    >
-      <ColumnsDirective>
-        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
-        {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
-      </ColumnsDirective>
-    </KanbanComponent>
-  </div>
-);
+const Kanban = () => {
+  // Define a function to handle the button click event
+  const handleButtonClick = (cardData) => {
+    // Perform the desired action when the button is clicked
+    console.log(`Button clicked for card with ID: ${cardData.Id}`);
+    // You can add your custom logic here
+  };
+
+  // Define a template for the Kanban card content
+  const cardTemplate = (cardData) => (
+    <div>
+      <h1>{cardData.Title}</h1>
+      <div>{cardData.Summary}</div>
+      <button type="button" onClick={() => handleButtonClick(cardData)}>Click Me</button>
+    </div>
+  );
+
+  return (
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header category="App" title="Kanban" />
+      <KanbanComponent
+        id="kanban"
+        keyField="Status"
+        dataSource={kanbanData}
+        cardSettings={{ template: cardTemplate }}
+      >
+        <ColumnsDirective>
+          {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+          {kanbanGrid.map((item, index) => <ColumnDirective key={index} {...item} />)}
+        </ColumnsDirective>
+      </KanbanComponent>
+    </div>
+  );
+};
 
 export default Kanban;
