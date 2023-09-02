8c4e57bf98f586bbd0bec4a26ebe4e5bbf558b0e
koen de vulder <devulderk@gmail.com>
2023-09-02 22:51:41 +0200

    feat: added nav

--- adf-solutions/src/main.tsx
+++ adf-solutions/src/main.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import Home from './pages/home/home.tsx'
 import './index.css'
+import { registerLicense } from '@syncfusion/ej2-base';
+
+// Registering Syncfusion license key
+registerLicense('Ngo9BigBOggjHTQxAR8/V1NGaF1cWGhAYVppR2NbfE5xfldGalxXVBYiSV9jS31Tf0VhWHxccHRTQmNbUQ==');
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
--- adf-solutions/src/pages/home/home.tsx
+++ adf-solutions/src/pages/home/home.tsx
@@ -1,8 +1,10 @@
+import SidebarWithMenu from '../../components/sidebar/sidebar-menu'
 import './home.css'
 
 function Home() {
   return (
     <>
+	<SidebarWithMenu></SidebarWithMenu>
      <h1 className="text-2xl font-bold underline">
 		Hello worsdadswald!
 	</h1>
