53696b38d1f6b6e607b8ba9c0d698d25adb45818
koen de vulder <devulderk@gmail.com>
2023-09-03 15:08:46 +0200

    feat: added outdir

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Change this to your desired output directory
+  },
 })
