1602a079058dda259c0d5344ed3b4386b75bbd98
Arnau Espin <arnauespin@gmail.com>
2023-09-03 21:27:39 +0200

    fix: sidebar autoclose

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -2,8 +2,8 @@ import { useRef } from "react";
 import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
 import { TreeViewComponent } from "@syncfusion/ej2-react-navigations";
 import { TextBoxComponent } from "@syncfusion/ej2-react-inputs";
-import "./sidebar.css";
 import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
+import "./sidebar.css";
 
 type SidebarProps = {
   onClick: (componentName: string) => void;
@@ -16,125 +16,125 @@ interface NodeId {
   };
 }
 
+const data: { [key: string]: unknown }[] = [
+  {
+    nodeId: "01",
+    nodeText: "Kanban",
+    iconCss: "icon-microchip icon",
+  },
+  {
+    nodeId: "02",
+    nodeText: "Deployment",
+    iconCss: "icon-thumbs-up-alt icon",
+  },
+  {
+    nodeId: "03",
+    nodeText: "Quick Start",
+    iconCss: "icon-docs icon",
+  },
+  {
+    nodeId: "04",
+    nodeText: "Components",
+    iconCss: "icon-th icon",
+    nodeChild: [
+      {
+        nodeId: "04-01",
+        nodeText: "Calendar",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "04-02",
+        nodeText: "DatePicker",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "04-03",
+        nodeText: "DateTimePicker",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "04-04",
+        nodeText: "DateRangePicker",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "04-05",
+        nodeText: "TimePicker",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "04-06",
+        nodeText: "SideBar",
+        iconCss: "icon-circle-thin icon",
+      },
+    ],
+  },
+  {
+    nodeId: "05",
+    nodeText: "API Reference",
+    iconCss: "icon-code icon",
+    nodeChild: [
+      {
+        nodeId: "05-01",
+        nodeText: "Calendar",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "05-02",
+        nodeText: "DatePicker",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "05-03",
+        nodeText: "DateTimePicker",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "05-04",
+        nodeText: "DateRangePicker",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "05-05",
+        nodeText: "TimePicker",
+        iconCss: "icon-circle-thin icon",
+      },
+      {
+        nodeId: "05-06",
+        nodeText: "SideBar",
+        iconCss: "icon-circle-thin icon",
+      },
+    ],
+  },
+  {
+    nodeId: "06",
+    nodeText: "Browser Compatibility",
+    iconCss: "icon-chrome icon",
+  },
+  {
+    nodeId: "07",
+    nodeText: "Upgrade Packages",
+    iconCss: "icon-up-hand icon",
+  },
+  {
+    nodeId: "08",
+    nodeText: "Release Notes",
+    iconCss: "icon-bookmark-empty icon",
+  },
+  {
+    nodeId: "09",
+    nodeText: "FAQ",
+    iconCss: "icon-help-circled icon",
+  },
+  {
+    nodeId: "10",
+    nodeText: "License",
+    iconCss: "icon-doc-text icon",
+  },
+];
+
 const Sidebar = ({ onClick, onToolbarPosChanged }: SidebarProps) => {
   const sidebarobj = useRef<SidebarComponent>(null);
-  const data: { [key: string]: unknown }[] = [
-    {
-      nodeId: "01",
-      nodeText: "Kanban",
-      iconCss: "icon-microchip icon",
-    },
-    {
-      nodeId: "02",
-      nodeText: "Deployment",
-      iconCss: "icon-thumbs-up-alt icon",
-    },
-    {
-      nodeId: "03",
-      nodeText: "Quick Start",
-      iconCss: "icon-docs icon",
-    },
-    {
-      nodeId: "04",
-      nodeText: "Components",
-      iconCss: "icon-th icon",
-      nodeChild: [
-        {
-          nodeId: "04-01",
-          nodeText: "Calendar",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "04-02",
-          nodeText: "DatePicker",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "04-03",
-          nodeText: "DateTimePicker",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "04-04",
-          nodeText: "DateRangePicker",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "04-05",
-          nodeText: "TimePicker",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "04-06",
-          nodeText: "SideBar",
-          iconCss: "icon-circle-thin icon",
-        },
-      ],
-    },
-    {
-      nodeId: "05",
-      nodeText: "API Reference",
-      iconCss: "icon-code icon",
-      nodeChild: [
-        {
-          nodeId: "05-01",
-          nodeText: "Calendar",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "05-02",
-          nodeText: "DatePicker",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "05-03",
-          nodeText: "DateTimePicker",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "05-04",
-          nodeText: "DateRangePicker",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "05-05",
-          nodeText: "TimePicker",
-          iconCss: "icon-circle-thin icon",
-        },
-        {
-          nodeId: "05-06",
-          nodeText: "SideBar",
-          iconCss: "icon-circle-thin icon",
-        },
-      ],
-    },
-    {
-      nodeId: "06",
-      nodeText: "Browser Compatibility",
-      iconCss: "icon-chrome icon",
-    },
-    {
-      nodeId: "07",
-      nodeText: "Upgrade Packages",
-      iconCss: "icon-up-hand icon",
-    },
-    {
-      nodeId: "08",
-      nodeText: "Release Notes",
-      iconCss: "icon-bookmark-empty icon",
-    },
-    {
-      nodeId: "09",
-      nodeText: "FAQ",
-      iconCss: "icon-help-circled icon",
-    },
-    {
-      nodeId: "10",
-      nodeText: "License",
-      iconCss: "icon-doc-text icon",
-    },
-  ];
-
   const width: string = "290px";
   const target: string = ".main-sidebar-content";
   const mediaQuery: string = "(min-width: 600px)";
@@ -145,7 +145,8 @@ const Sidebar = ({ onClick, onToolbarPosChanged }: SidebarProps) => {
     child: "nodeChild",
     iconCss: "iconCss",
   };
-  const toolbarCliked = (): void => {
+
+  const toolbarClicked = (): void => {
     sidebarobj.current!.toggle();
     onToolbarPosChanged(sidebarobj.current?.isOpen);
   };
@@ -175,7 +176,7 @@ const Sidebar = ({ onClick, onToolbarPosChanged }: SidebarProps) => {
         {/* toggle the sidebar */}
         <div>
           <ButtonComponent
-            onClick={toolbarCliked.bind(this)}
+            onClick={toolbarClicked.bind(this)}
             className="toggle-sidebar-button"
             cssClass="e-flat e-primary"
           >
@@ -190,6 +191,8 @@ const Sidebar = ({ onClick, onToolbarPosChanged }: SidebarProps) => {
           target={target}
           mediaQuery={mediaQuery}
           isOpen={true}
+          closeOnDocumentClick={false}
+          close={false}
         >
           <div className="res-main-menu">
             <div className="table-content">
@@ -201,11 +204,13 @@ const Sidebar = ({ onClick, onToolbarPosChanged }: SidebarProps) => {
             </div>
             <div>
               <TreeViewComponent
+
                 id="mainTree"
                 cssClass="main-treeview"
                 fields={fields}
                 expandOn="Click"
                 nodeSelected={handleNodeClick}
+                autoCheck={false}
               />
             </div>
           </div>
