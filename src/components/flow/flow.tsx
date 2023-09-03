cf3ffb54b0ed1e8d992bc8e979a0b13d2747c8e6
koen de vulder <devulderk@gmail.com>
2023-09-03 18:45:47 +0200

    feat: add button

--- src/components/flow/flow.tsx
+++ src/components/flow/flow.tsx
@@ -1,4 +1,4 @@
-import { useCallback } from 'react';
+import { useCallback } from "react";
 import ReactFlow, {
   MiniMap,
   Controls,
@@ -9,10 +9,12 @@ import ReactFlow, {
   Connection,
   Edge,
   BackgroundVariant,
-} from 'reactflow';
+  Panel,
+} from "reactflow";
 
 import "reactflow/dist/style.css";
 import "./flow.css";
+import { ButtonComponent } from "@syncfusion/ej2-react-buttons";
 
 const initialNodes = [
   { id: "1", position: { x: 0, y: 0 }, data: { label: "1" } },
@@ -24,7 +26,19 @@ function Flow() {
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect = useCallback((params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+  const onConnect = useCallback(
+    (params: Edge | Connection) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
+
+  const onAdd = () => {
+    const newNode = {
+      id: (nodes.length + 1).toString(),
+      position: { x: 0, y: 0 },
+      data: { label: (nodes.length + 1).toString() },
+    };
+    setNodes((nodes) => nodes.concat(newNode));
+  };
 
   return (
     <div style={{ width: "100%", height: "100vh" }}>
@@ -35,6 +49,18 @@ function Flow() {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
       >
+        <Panel position="top-right">
+          <div className="col-xs-12 col-sm-12 col-lg-6 col-md-6">
+            <div className="col-xs-12 col-sm-12 col-lg-6 col-md-6">
+              <ButtonComponent
+                cssClass="e-small e-round"
+                iconCss="e-btn-sb-icons e-add-icon"
+                isPrimary
+                onClick={onAdd}
+              ></ButtonComponent>
+            </div>
+          </div>
+        </Panel>
         <Controls />
         <MiniMap />
         <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
