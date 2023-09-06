44e55557caba6932765f2765dd6e5ad895768738
koen de vulder <devulderk@gmail.com>
2023-09-06 18:37:47 +0200

    feat: added styling

--- src/components/flow/card-types/default-node.tsx
+++ src/components/flow/card-types/default-node.tsx
@@ -1,4 +1,3 @@
-import { useCallback } from 'react';
 import { Handle, Position } from 'reactflow';
 import "./default-node.css";
 
@@ -7,17 +6,13 @@ interface DefaultNodeProps {
 	data: any; //todo: type this
 }
 
-function DefaultNode({ isConnectable }: DefaultNodeProps) {
-	const onChange = useCallback((evt: React.ChangeEvent<HTMLInputElement>) => {
-		console.log(evt.target.value);
-	}, []);
-
+function DefaultNode({ isConnectable, data }: DefaultNodeProps) {
 	return (
 		<>
-			<div className="Base-Node" >
-				<div>
-					<label htmlFor="text">Text:</label>
-					<input id="text" name="text" onChange={onChange} className="nodrag" />
+			<div >
+				<div className="Base-Node flex flex-col items-center" >
+					<div className="m-1"><h1>{data.title}</h1></div>
+					<div className="m-1"><h1>{data.subject}</h1></div>
 				</div>
 				<Handle type="target" position={Position.Top} id="a" isConnectable={isConnectable} />
 				<Handle type="source" position={Position.Bottom} id="b" isConnectable={isConnectable} />
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{html,js,ts,tsx}"],
+  content: ["./**/*.{html,js,ts,tsx}"],
   theme: {
     extend: {},
   },
